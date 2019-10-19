2cccda27a3d38ab05108676b71c56f391bce136a
mach <mach1982@gmail.com>
2019-10-19 13:55:14 +0100

    adding fb bot

---
+++ fb_chat_bot.py
@@ -0,0 +1,19 @@
+from fbchat import Client
+from fbchat.models import *
+
+
+
+username = input("Facebook user ID or e-mail:")
+password = input("Enter in you Facebook Password:")
+# login
+
+client = Client(username, password)
+users = list(client.fetchThreadList())
+
+for i in range(0,len(users)):
+    if users[i].name =='Lorcan Mac Hugh':
+        
+        client.send(Message(text='This a test from my Facbook Bot'), thread_id=users[i].uid, thread_type=ThreadType.USER)
+    
+
+client.logout()
